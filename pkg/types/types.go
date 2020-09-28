8787b39ac5fee448c7589cac8f9098005bb4a405
bahrullo <bahrom656>
2020-09-28 12:41:36 +0500

    Добавлена Status в Payment

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -3,12 +3,22 @@ package types
 //Money представляет собой денежную сумму в минимальных единицах {центы, копейки, дирамы, и т.д.}
 type Money int64
 
+//Status представляет собой статус платежа
+type Status string
+
 //Category представляет собой категорию, в котором был совершен платёж
 type Category string
 
+//Предопределенные статусы платежей
+const (
+	StatusOk Status	= "OK"
+	StatusFail Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
 //Payment представляет информацию о платеже
 type Payment struct{
 	ID 			int
 	Amount		Money
 	Category 	Category
+	Status		Status		
 }
